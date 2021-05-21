97938d39c7a3abef4102cb640452ed979f12b899
Gowri Sankar <sankaryadavrgukt@hotmail.com>
2021-05-22 00:28:47 +0530

    installed firebase and also created project in firebase console

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDD5LtpXUyhiIFyCG5aY5FkvuCdvu4Lzj8",
+    authDomain: "rgukt-alumni.firebaseapp.com",
+    projectId: "rgukt-alumni",
+    storageBucket: "rgukt-alumni.appspot.com",
+    messagingSenderId: "948012823600",
+    appId: "1:948012823600:web:b499d7f0a7ab27930ea9f5",
+    measurementId: "G-6HD6WYSB8V"
+};
+  
+firebaseConfig.initializeApp(firebaseConfig)
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+
+export default firebase;
