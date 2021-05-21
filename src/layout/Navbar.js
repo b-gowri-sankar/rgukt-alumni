e908a297839acd23249661438ac37e15a7b20842
Gowri Sankar <sankaryadavrgukt@hotmail.com>
2021-05-21 13:17:13 +0530

    NavBar Completed (No Links to NavBar)

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -4,7 +4,31 @@ import React from 'react';
 const navBar = () => {
     return (
         <Nav>
-
+            <Logo>
+                <span>
+                    RGUKTAA
+                </span>
+            </Logo>
+            <NavMenu>
+            <a href="/">
+              <span>HOME</span>
+            </a>
+            <a>
+              <span>LIBRARY</span>
+            </a>
+            <a>
+              <span>CAREERS</span>
+            </a>
+            <a>
+              <span>PLACEMENT CELL</span>
+            </a>
+            <a>
+              <span>CAMPUSES</span>
+            </a>
+            <a>
+              <span>CONTACT US</span>
+            </a>
+          </NavMenu>
         </Nav>
     )
 }
@@ -22,6 +46,71 @@ const Nav = styled.nav`
     padding: 0 36px;
     letter-spacing: 16px;
     z-index: 3;
+`;
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  display: inline-block;
+  color:white;
+  span {
+    color:white;
+    padding:10px;
+    letter-spacing: 0.3rem;
+  }
+`;
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+  /* @media (max-width: 768px) {
+    display: none;
+  } */
 `;
 
 export default navBar;
