2dc281127544061632287405d3194f536d3b1905
Gowri Sankar <sankaryadavrgukt@hotmail.com>
2021-05-22 23:09:42 +0530

    Completed Posts

--- src/PostsEvents/Posts/Post/PostDetails.js
+++ src/PostsEvents/Posts/Post/PostDetails.js
@@ -1,15 +1,47 @@
 import classes from './PostDetails.module.css'
 import React from 'react'
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux'
 
-function PostDetails() {
+const PostDetails=(props)=>{
     console.log('we are in the postdetails')
-    return (
+    const { post } = props;
+    console.log(post)
+    if (post) {
+        return (
+            <div className={classes.PostDetails}>
+                <h1>{ post.title}</h1>
+                <p>Posted Date:</p>
+                <p>{post.content}</p>
+            </div>
+        )
+    }
+    else {
+        return (
+        // here you can use spinner
         <div className={classes.PostDetails}>
-            <h1>Title</h1>
-            <p>Posted Date:</p>
-            <p>Description</p>
+            <h4>It is still Loading ....</h4>
         </div>
-    )
+        )
+        
+    }
 }
 
-export default PostDetails
+const mapStateToProps = (state, ownProps ) => {
+    const id = ownProps.match.params.id;
+    const posts = state.firestore.data.posts;
+    const post = posts ? posts[id] : null;
+    // console.log(centers)
+    // console.log(id)
+    return {
+        post: post
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection:'posts'}
+    ])
+)(PostDetails)
