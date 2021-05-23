787fde1473ea5e7b07c6bc6b671d08304fb64fd0
Gowri Sankar <sankaryadavrgukt@hotmail.com>
2021-05-23 08:27:03 +0530

    Events: GET Data from firebase Database

--- src/PostsEvents/Events/Events.js
+++ src/PostsEvents/Events/Events.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import Event from './Event/Event';
 import classes from './Events.module.css';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import {Link} from 'react-router-dom'
 
-const Events = () => {
+const Events = (props) => {
+    const events = props.event;
     return (
         <div className={classes.Events}>
             <p className={classes.EventsTitle}>Events</p>
+            { events && events.map((event) => (
+                <Link to={'/event/'+event.id}>
+                    <Event event={event} id={event.id} />
+                    </Link>
+            ))}
+            {/* <Event />
             <Event />
-            <Event />
-            <Event />
+            <Event /> */}
         </div>
     );
 }
 
-export default Events;
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        // center: state.center.projects for demo purposes
+        event:state.firestore.ordered.events
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection:'events'}
+    ])
+)(Events)
