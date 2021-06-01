1fb2428b7e129b0ef9ea4a882c46928b0343b3db
Gowri Sankar <sankaryadavrgukt@hotmail.com>
2021-06-01 22:05:17 +0530

    profile design

--- src/Profiles/Profile/Profile.js
+++ src/Profiles/Profile/Profile.js
@@ -1,15 +1,24 @@
 import React from 'react';
-
+import classes from './Profile.module.css'
 import { Link } from 'react-router-dom'
 
 const Profile = (props) => {
     return (
         <Link to={'profile/'+props.doc.id}>
-        <div>
-            <p>{ props.doc.name}</p>
-            <p>{props.doc.batch}</p>
-            <p>{props.doc.company}</p>
-            <img width="100" height="100" src={props.doc.avatar} alt={props.doc.id} />
+            <div className={classes.card}>
+                <div className={ classes.profilesidebar}>
+                    <img
+                        className={classes.profileimage}
+                        width="100"
+                        height="100"
+                        src={props.doc.avatar}
+                        alt={props.doc.id} />
+                </div>
+                <div className={classes.profilemain}>
+                    <h2 className={classes.profilename}>{ props.doc.name}</h2>
+                    <p class={classes.profileposition}>{props.doc.branch}</p>
+                    <p class={classes.profilebody}>{props.doc.company}</p>
+                </div>
             </div>
         </Link>
     )
